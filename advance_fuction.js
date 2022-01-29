1c7c2e120d98cda92853d73a2933aaf933c3c851
anamerasul <anamerasul@gmail.com>
2022-01-29 18:34:14 +0600

    adv function

---
+++ advance_fuction.js
@@ -0,0 +1,14 @@
+// parameter
+
+function bringSingara(taka){
+
+               console.log("singara er jnno dise" ,taka);
+               console.log('mama singara den');
+               var singaraPrice =10;
+               var singaraQuantity = taka/ singaraPrice;
+               return singaraQuantity ;
+}
+var money =90;
+var singara= bringSingara(money);
+
+console.log('ai nen singara' , singara)
