b19eedad45231428a92433b565a469f3a640be68
anamerasul <anamerasul@gmail.com>
2022-01-29 17:17:13 +0600

    function

---
+++ function.js
@@ -0,0 +1,19 @@
+// function declaretion
+function turnOnFan(){
+               console.log('walk towards the switch');
+               console.log('presh  the switch');
+
+}
+
+// call function
+
+turnOnFan();
+
+console.log('eat breakfast');
+console.log('drink tea');
+turnOnFan();
+
+var country ="bd";
+var city ="Dhaka";
+var loction="bogra";
+turnOnFan();
