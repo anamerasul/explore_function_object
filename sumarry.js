af22a66996bd152b23ad623fccc6cfd9b2d4f707
anamerasul <anamerasul@gmail.com>
2022-01-29 20:44:42 +0600

    all add

---
+++ sumarry.js
@@ -0,0 +1,43 @@
+var skyColor ='white';
+var phones =['ipone','xaomi','samgsanug','lg','htc'];
+
+phones[3]='waltop';
+
+// check an element in arry
+
+// if(phones.indexOf('oppo')==-1){
+//                console.log('missing')
+// }
+// if(phones.indexOf('lg') !=-1){
+//                console.log("lg available now")
+// }
+
+// loop
+// var num =0;
+// while (num<=10){
+//                num++;
+// }
+// for (var i =0 ; i<phones.length; i++){
+
+// }
+
+
+// function
+
+function addThreenumbers(num1,num2,num3){
+               var total =num1+num2+num3;
+               return total;
+}
+
+
+var result = addThreenumbers(87,41,56);
+
+console.log(result);
+
+// object 
+
+var microphone={
+               brand:'blue yeti',
+               price:120,
+               color:'black'
+}
