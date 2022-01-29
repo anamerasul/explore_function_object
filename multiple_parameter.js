e16c3d0c20731695738a93c4c9607561190a1563
anamerasul <anamerasul@gmail.com>
2022-01-29 19:04:01 +0600

    more parameter

---
+++ multiple_parameter.js
@@ -0,0 +1,45 @@
+function addTwoNumbers(number1,number2){
+               console.log(number1,number2);
+               // console.log(number2);
+               var total =number1+number2;
+               return total;
+
+}
+
+var firstNumer =35;
+var secondNumer=47
+addTwoNumbers(secondNumer,firstNumer);
+var result =addTwoNumbers(secondNumer,firstNumer);
+console.log(result);
+
+
+function multiplyTwoNumbers(num1,num2){
+               var total =num1*num2;
+               return total;
+}
+
+var result = multiplyTwoNumbers(5,100);
+console.log(result)
+
+// var substrat 
+
+function subtractTwonumbers(num1,num2){
+               var total=num1-num2;
+               return total;
+               
+}
+
+var result =subtractTwonumbers(100,20)
+
+console.log(result);
+
+// var divided
+function dividedTwonumbers(num1,num2){
+               var total=num1/num2;
+               return total;
+               
+}
+
+var result =dividedTwonumbers(100,20)
+
+console.log(result);
